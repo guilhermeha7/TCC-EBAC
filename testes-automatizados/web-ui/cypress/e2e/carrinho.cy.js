e2fcfbdd78be024f553d69341441f9db26402d25
Guilherme <guilhermeha77@outlook.com>
2024-11-26 13:51:57 -0300

    Teste do Carrinho Otimizado

--- testes-automatizados/web-ui/cypress/e2e/carrinho.cy.js
+++ testes-automatizados/web-ui/cypress/e2e/carrinho.cy.js
@@ -1,32 +1,20 @@
 /// <reference types="cypress"/>
-import NavBarPage from '../pages/nav-bar.page'
-import ProdutosPage from '../pages/produtos.page'
 import ProdutoEscolhidoPage from '../pages/produto-escolhido'
 
 describe('Funcionalidade: Carrinho', () => {
-
   it('Deve adicionar 3 produtos ao carrinho', () => {
     cy.visit('/')
 
-    //Adição do Primeiro Produto ao Carrinho
-    NavBarPage.clicarEmComprar()
-    ProdutosPage.clicarNoProduto(2)
-    ProdutoEscolhidoPage.escolherCaracteristicas('S','Blue','2')
-    ProdutoEscolhidoPage.clicarEmComprar()
+    //Adição do 1º Produto ao Carrinho
+    cy.addProdutoAoCarrinho(2,'S','Blue','2')
     cy.get('.woocommerce-message').should('contain','foram adicionados no seu carrinho')
 
-    //Adição do Segundo Produto ao Carrinho
-    NavBarPage.clicarEmComprar()
-    ProdutosPage.clicarNoProduto(4)
-    ProdutoEscolhidoPage.escolherCaracteristicas('32','Black','1')
-    ProdutoEscolhidoPage.clicarEmComprar()
+    //Adição do 2º Produto ao Carrinho
+    cy.addProdutoAoCarrinho(4,'32','Black','1')
     cy.get('.woocommerce-message').should('contain','foi adicionado no seu carrinho')
 
-    //Adição do Terceiro Produto ao Carrinho
-    NavBarPage.clicarEmComprar()
-    ProdutosPage.clicarNoProduto(0)
-    ProdutoEscolhidoPage.escolherCaracteristicas('XS','Black','3')
-    ProdutoEscolhidoPage.clicarEmComprar()
+    //Adição do 3º Produto ao Carrinho
+    cy.addProdutoAoCarrinho(0,'XS','Black','3')
     cy.get('.woocommerce-message').should('contain','foram adicionados no seu carrinho')
 
     //Abrir Carrinho
